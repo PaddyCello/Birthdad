00de736776158fe7ced5a7611ee31e4829260d45
Patrick Johnson <paddyjohnsonmusic@gmail.com>
2021-01-27 16:55:14 +0000

    done

--- Desktop/birthdad/birthdad.js
+++ Desktop/birthdad/birthdad.js
@@ -33,24 +33,23 @@ const checkInfo = () => {
 const todayMonth = today.getMonth();
 const todayDay = today.getDate();
 
-console.log(typeof inputDate);
-
 const birthdayMonth = inputDate.getMonth();
 const birthdayDay = inputDate.getDate();
 
 const numOfCandles = today.getFullYear() - inputDate.getFullYear();
-console.log(numOfCandles);
+
   if (todayMonth == birthdayMonth && todayDay == birthdayDay) {
     
     document.querySelector('h1').innerText = `Congratulations, ${inputName}! You are ${numOfCandles} years old!`;
-    document.querySelectorAll('div').hidden = true;
+    document.getElementById('namein').style.display = 'none';
+    document.getElementById('datein').style.display = 'none';
     document.querySelector('body').style.backgroundImage = shaun;
   } else {
     document.querySelector('h1').innerText = `Commiserations, ${inputName}.  It's not your birthday.`;
     document.querySelector('body').style.backgroundImage = sadness;
-    document.querySelector('h1').style.fontFamily = 'Blackletter';
-    person.value = '';
-  birthdate.value = '';
+    document.getElementById('namein').style.display = 'none';
+    document.getElementById('datein').style.display = 'none';
+    document.querySelector('h1').style.fontFamily = "'Potta One', cursive";
   };
 };
 
